dd8604c6226527bc86dc89938b2421673a3d9ea7
Ahmed Al-Ganad <ahmedbaligh100@gmail.com>
2024-06-07 18:08:35 +0200

    Notification_sender class implemented

--- SmartWatch/Notification_sender.h
+++ SmartWatch/Notification_sender.h
@@ -6,8 +6,8 @@
 
 const char* ssid = "K9";                           //hardcoded
 const char* password = "saifceo20";                // hardcoded
-const char* serverAddress = "192.168.164.235";  // Raspberry Pi's IP address hardcoded
-const int serverPort = 8080;                   // Servers Port Number  hardcoded
+const char* serverAddress = "192.168.164.235";     // Raspberry Pi's IP address hardcoded
+const int serverPort = 8080;                       // Servers Port Number  hardcoded
 const char* watch_code = "2114";
 
 
@@ -16,6 +16,8 @@ class Notification_sender{
   private:
 
   public:
+
+  WiFiClient client;
 
   char* full_name = "";
 
@@ -26,7 +28,7 @@ class Notification_sender{
 
   void connect_to_server();     // also send the watch_code to the server in this function
 
-  void send_notification_to_contact_person();   //Send ->  "The user " + full_name + " has had a siegure in " + "coordinates: Longitude: " longitude + " " + "Latitude: " latitude
+  void send_notification_to_contact_person(float longitude, float latitude);   //Send ->  "The user " + full_name + " has had a siegure in " + "coordinates: Longitude: " longitude + " " + "Latitude: " latitude
 
 
 };
