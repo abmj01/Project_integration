5c37e7633becdfe9fcea143e32e522dd521cba1c
MarcelDaBine <rafael_balan04@yahoo.com>
2024-06-08 17:22:19 +0200

    passed the send notifications to contact persons test and retrieve the full_name based on the watch code test

---
+++ SmartWatch/Notification_sender.cpp
@@ -0,0 +1,67 @@
+#include "Notification_sender.h"
+
+const char* ssid = "DIGI-bE5h";                           //hardcoded
+const char* password = "5zVGwK92";                // hardcoded
+const char* serverAddress = "192.168.100.227";     // Raspberry Pi's IP address hardcoded
+const int serverPort = 8080;                       // Servers Port Number  hardcoded
+const char* watch_code = "2580";
+
+// Constructor
+Notification_sender::Notification_sender() {
+}
+
+// Method to initialize WiFi
+void Notification_sender::initialize_WiFi() {
+    Serial.print("Connecting to ");
+    Serial.println(ssid);
+    WiFi.begin(ssid, password);
+
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(100);
+        Serial.print(".");
+    }
+    Serial.println("Connected to WiFi");
+}
+
+// Method to connect to the server and send the initial message
+void Notification_sender::connect_to_server() {
+    if (!client.connect(serverAddress, serverPort)) {
+        Serial.println("Failed to connect to server!");
+        delay(100);
+        return;
+    }
+
+    client.print(String("0 ") + watch_code);
+    full_name = waitForServerResponse();
+    Serial.println("Watch code sent to server, name associated: " + full_name);
+}
+
+// Method to send notification to the contact person
+void Notification_sender::send_notification_to_contact_person(float longitude, float latitude) {
+    if (!client.connect(serverAddress, serverPort)) {
+        Serial.println("Failed to connect to server!");
+        delay(100);
+        return;
+    }
+    String message = "1 The user " + full_name + " has had a seizure in coordinates: Longitude: " + String(longitude, 6) + " Latitude: " + String(latitude, 6);
+    client.print(message);
+    Serial.println("Notification sent to contact person: " + message);
+    String output = waitForServerResponse();
+    Serial.println("Server response: " + output);
+}
+
+// Private helper method to wait for server response
+String Notification_sender::waitForServerResponse() {
+    unsigned long timeout = millis() + 5000; // 5 seconds timeout
+    while (!client.available() && millis() < timeout) {
+        delay(100);
+    }
+
+    if (client.available()) {
+        // Receive response from server
+        String response = client.readStringUntil('\n');
+        return response;
+    } else {
+        return "Timeout waiting for server response";
+    }
+}
--- SmartWatch/Notification_sender.h
+++ SmartWatch/Notification_sender.h
@@ -3,34 +3,22 @@
 
 #include <WiFi.h>
 
+extern const char* ssid;
+extern const char* password;
+extern const char* serverAddress;
+extern const int serverPort;
+extern const char* watch_code;
 
-const char* ssid = "K9";                           //hardcoded
-const char* password = "saifceo20";                // hardcoded
-const char* serverAddress = "192.168.164.235";     // Raspberry Pi's IP address hardcoded
-const int serverPort = 8080;                       // Servers Port Number  hardcoded
-const char* watch_code = "2114";
+class Notification_sender {
+public:
+    WiFiClient client;
+    String full_name = "";
 
-
-class Notification_sender{
-
-  private:
-
-  public:
-
-  WiFiClient client;
-
-  String full_name = "";
-
-  
-  Notification_sender();     //constructor
-
-  void initialize_WiFi();
-
-  void connect_to_server();     // also send the watch_code to the server in this function
-
-  void send_notification_to_contact_person(float longitude, float latitude);   //Send ->  "The user " + full_name + " has had a siegure in " + "coordinates: Longitude: " longitude + " " + "Latitude: " latitude
-
-
+    Notification_sender();     //constructor
+    void initialize_WiFi();
+    void connect_to_server();  // also send the watch_code to the server in this function
+    void send_notification_to_contact_person(float longitude, float latitude);   //Send ->  "The user " + full_name + " has had a siegure in " + "coordinates: Longitude: " longitude + " " + "Latitude: " latitude
+    String waitForServerResponse();
 };
 
-#endif
+#endif // NOTIFICATION_SENDER_H
